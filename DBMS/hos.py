e1d253f73c70dab90aec2bce95f0af61aa8a4d54
aryan-0102 <89297308+aryan-0102@users.noreply.github.com>
2024-04-08 20:31:36 +0530

    Create hos.py

---
+++ DBMS/hos.py
@@ -0,0 +1,6 @@
+
+from google.cloud import storage 
+from datetime import datetime
+import sys
+ mysql.connector.connect(user='me', password='aryan', host='34.170.231.29', 
+                              database='db')
